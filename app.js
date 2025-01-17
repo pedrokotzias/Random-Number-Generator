8af12cd4b6bd23f250b95eb2ed38a20ae547b6e7
Pedro Kotzias <pepeuk@gmail.com>
2025-01-17 12:52:19 -0300

    Projeto Sorteador de Número

---
+++ app.js
@@ -0,0 +1,97 @@
+let generatedNumbers = [];
+
+function getDomId(id) {
+    return document.getElementById(id);
+}
+
+function generateRandomNumbers() {
+    let totalOfNumbers = parseInt(getDomId('quantidade').value);
+    let fromNumber = parseInt(getDomId('de').value);
+    let upToNumber = parseInt(getDomId('ate').value);
+
+    if (totalOfNumbers > (upToNumber - fromNumber + 1)) {
+        alert('A quantidade de números a serem sorteados não pode ser maior que o valor "Até o número",\n' +
+            'ou o valor "Do número" não pode ser maior que o valor "Até o número"');
+        return;
+    }
+
+    while (generatedNumbers.length < totalOfNumbers) {
+        const randomNumber = Math.floor(Math.random() * (upToNumber - fromNumber + 1)) + fromNumber;
+        if (!generatedNumbers.includes(randomNumber)) {
+            generatedNumbers.push(randomNumber);
+        }
+    }
+    return generatedNumbers;
+}
+
+function sortNumbers() {
+    generatedNumbers.sort((a, b) => a - b);
+    return generatedNumbers;
+}
+
+function showResultsText() {
+    if (isGeneratedNumbersEmpty()) {
+        return;
+    }
+    sortNumbers();
+    getDomId('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados: ${generatedNumbers.join(', ')}</label>`;
+}
+
+function originalTextResult() {
+    getDomId('resultado').innerHTML = `<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>`;
+}
+
+function clearInputValues() {
+    getDomId('quantidade').value = '';
+    getDomId('de').value = '';
+    getDomId('ate').value = '';
+}
+
+// function allowReset() {
+//     if (isGeneratedNumbersEmpty()) {
+//         return;
+//     }
+//     let restartButton = getDomId('btn-reiniciar');
+//     restartButton.classList.remove('container__botao-desabilitado');
+//     restartButton.classList.add('container__botao');
+// }
+
+// function disableReset() {
+//     let restartButton = getDomId('btn-reiniciar');
+//     restartButton.classList.remove('container__botao');
+//     restartButton.classList.add('container__botao-desabilitado');
+// }
+
+function toggleResetButton(isEnabled) {
+    const restartButton = getDomId('btn-reiniciar');
+
+    if (isEnabled) {
+        restartButton.classList.remove('container__botao-desabilitado');
+        restartButton.classList.add('container__botao');
+    } else {
+        restartButton.classList.remove('container__botao');
+        restartButton.classList.add('container__botao-desabilitado');
+    }
+}
+
+function sortear() {
+    generatedNumbers = [];
+    generateRandomNumbers();
+    showResultsText();
+    toggleResetButton(!isGeneratedNumbersEmpty()); 
+    // allowReset();
+}
+
+function reiniciar() {
+    generatedNumbers = [];
+    clearInputValues();
+    originalTextResult();
+    toggleResetButton(false);
+    // disableReset();
+}
+
+function isGeneratedNumbersEmpty() {
+    return generatedNumbers.length === 0; 
+}
+
+getDomId('btn-sortear').addEventListener('click', sortear);
